3472d066872d035a6260644c234866104026397d
Viera Garcia <vieravfg@gmail.com>
2021-06-28 18:34:46 -0400

    more

---
+++ static/js/app.js
@@ -0,0 +1,131 @@
+function init(){
+
+     //read the data
+     d3.json("samples.json").then((data)=> {
+        console.log(data);
+
+        // appending ids on dropdown
+        data.names.forEach((id) => {
+            d3.select("#selDataset")
+            .append("option")
+            .text(id)
+            .property("value");
+
+
+        });
+        // Choosing subject 940 
+        console.log(data.names[0]);
+        buildbar(data.names[0]);
+        buildbubble(data.names[0])
+
+    });
+};
+init();
+
+
+// Call optionChanged when a change takes place to the DOM
+d3.selectAll("body").on("change", optionChanged);
+// Function is called when a OTU is selected from dropdown menu
+function optionChanged() {
+    // Use d3 to select dropdown menu
+    var dropdownmenu = d3.select("#selDataset");
+    // assign the value of the dropdown menu option to a variable
+    var selectedoption = dropdownmenu.property("value");
+
+    // Build the plot with the new selected option 
+    buildbar(selectedoption);
+    buildbubble(selectedoption);
+}
+
+
+function buildbar(selectedoption){
+    d3.json("samples.json").then((data)=> {
+        var sample_data = data.samples.filter(object => object.id.toString() === selectedoption)[0];
+        console.log(sample_data)
+        // Slice the first 10 objects for sample_values
+        var sliced_sample = (sample_data.sample_values.slice(0, 10)).reverse();
+        // Slice the first 10 objects for otu_ids
+        var sliced_otu_ids = (sample_data.otu_ids.slice(0,10)).reverse(); // 0 is inclusive, 10 is not. 
+        console.log(sliced_otu_ids);
+        // Slice the first 10 labels
+        var slice_labels = (sample_data.otu_labels.slice(0, 10)).reverse();
+        console.log(slice_labels);
+
+        // Create a trace variable
+        var trace = {
+            x: sliced_sample,
+            y: sliced_otu_ids.map(object => "OTU" + object),
+            text: slice_labels,
+            name: "OTU",
+            type: "bar",
+            orientation: "h",
+            marker: {color: "pink"},
+        };
+        var bardata = [trace];
+        // Apply the group bar mode to the layout
+        var layout = {
+            title: "Top 10 Bacteria Coultures Found",
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 100,
+            }
+        };
+        
+        Plotly.newPlot("bar", bardata, layout);   
+        
+    });
+    
+
+};
+
+function buildbubble(selectedoption){
+    d3.json("samples.json").then((data)=> {
+        var sample_data = data.samples.filter(object => object.id.toString() === selectedoption)[0];
+        var otu_ids = sample_data.otu_ids;
+        var sample_val = sample_data.sample_values;
+
+        // Create a trace variable 
+        var trace = {
+            x: otu_ids,
+            y: sample_val,
+            text: sample_data.otu_labels,
+            mode: 'markers',
+            marker: {
+                size: sample_val,
+                color: otu_ids,
+                colorscale: 'Picnic',
+                showscale: true,
+                colorbar: {
+                    thickness: 10,
+                    y: 0.5,
+                    ypad: 0,
+                    titleside: 'bottom',
+                    outlinewidth: 1,
+                    tickfont: {
+                      family: 'Lato',
+                      size: 14,
+                      color: 'pink'
+                    }
+                } 
+            }
+        }
+        var bubbledata = [trace];
+  
+        // Customize Layout 
+        var layout = {
+            xaxis:{title: "OTU ID"},
+            title: "Bacteria Cultures Per Sample",
+            height: 500,
+            width: 1000,
+        }
+
+        // Create the bubble plot
+        Plotly.newPlot("bubble", bubbledata, layout); 
+    });
+}
+
+
+
+
