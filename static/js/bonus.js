0c7ef56484ca06f2a6356f332ac03dbe10ecc80c
Viera Garcia <vieravfg@gmail.com>
2021-06-28 23:19:25 -0400

    gauge

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -6,21 +6,22 @@ function buildgauge(wfreq) {
           type: "indicator",
           mode: "gauge+number",
           value: wfreq,
-          title: { text: "Scrubs per Week", font: { size: 34 } },
+          title: { text: "Scrubs per Week" , font: { size: 19 } },
+          number: { font: { size: 62, color:'cornflowerblue' }},
           gauge: {
-            axis: { range: [null, 9], tickwidth: 1, tickcolor: "darkblue", nticks:10 },
-            bar: { color: "darkblue" },
+            axis: { range: [null, 9], tickwidth: 1, tickcolor: "red", nticks:10 },
+            bar: { color: "LightCoral" },
             bgcolor: "white",
             borderwidth: 2,
-            bordercolor: "yellow",
+            bordercolor: "Lavender",
             steps: [
               { range: [0, 4.5], color: "cyan" },
               { range: [4.5, 9], color: "orange" }
             ],
             threshold: {
               line: { color: "yellow", width: 4 },
-              thickness: 0.75,
-              value: 490
+              thickness: 1.8,
+              value: wfreq
             }
           }
         }
@@ -30,7 +31,7 @@ var layout = {
         width: 500,
         height: 400,
         margin: { t: 25, r: 25, l: 25, b: 25 },
-        font: { color: "darkblue", family: "Arial" }
+        font: { color: "Gray", family: "Arial"}
     };
       
 
